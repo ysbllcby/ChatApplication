61e6579429dce89677b9b2fae34efd50ad671d23
ysabellacby <111803960+myscby@users.noreply.github.com>
2023-01-14 13:21:28 -0700

    LogIn Screen

--- app/src/main/java/com/example/chatapplication/Login.kt
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -1,11 +1,32 @@
 package com.example.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class Login : AppCompatActivity() {
+
+    private lateinit var edt_email: EditText
+    private lateinit var edt_password: EditText
+    private lateinit var btn_login: Button
+    private lateinit var btn_signup: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //Initialize fields
+        edt_email = findViewById(R.id.edt_email)
+        edt_password = findViewById(R.id.edt_password)
+        btn_login = findViewById(R.id.btn_login)
+        btn_signup = findViewById(R.id.btn_signup)
+
+        //Redirect user to SignUp page
+        btn_signup.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
